8b73d51cc8fb2df01dc17bce0905cf51302e0ce6
dabla1710 <daniel.blaich@outlook.com>
2024-05-07 11:45:39 +0200

    amd and java setup added
    First java setup for school  on nixos

---
+++ amd/gnome/nixos/configuration.nix
@@ -0,0 +1,236 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # --- ntfs support
+  boot.supportedFilesystems = [ "ntfs" ];
+
+  # --- AMD GPU
+  boot.initrd.kernelModules = [ "amdgpu" ]; # use amd early
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "amdgpu" ]; # AMDGPU
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "de";
+    xkbVariant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "de";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.dabs = {
+    isNormalUser = true;
+    description = "dabs";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.fish;
+    packages = with pkgs; [
+      firefox
+      flatpak
+    #  thunderbird
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+
+  # Unsafe electron for obsidian
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-25.9.0"
+  ];
+
+
+  environment.systemPackages = with pkgs; [
+  # shells
+  fish
+  # terminals
+  alacritty
+  kitty
+  # Commandline Utilities
+  lf # inspired by ranger
+  lshw # listing hardware
+  git
+  gh
+  neovim
+  helix
+  wl-clipboard
+  xclip
+  neofetch
+  tree
+  unzip
+  # Development Programming Languages
+  python3
+  #clang
+  gcc
+  gnumake
+  cmake
+  go
+  rustup
+  zig
+  # java stuff
+  jdk8
+  maven # package manager
+  # Language Servers
+  zls
+  gopls
+  vscode-langservers-extracted
+  vimPlugins.nvim-jdtls # java
+  jdt-language-server # java
+  # IDEs / Editors
+  vscode
+  jetbrains.clion
+  jetbrains.goland
+  jetbrains.idea-ultimate
+  jetbrains.rider
+  jetbrains.pycharm-professional   #jetbrains.pycharm-community
+  eclipses.eclipse-java
+  # gnome
+  gnome.gnome-tweaks
+  gnome.gnome-keyring
+  gnome-extension-manager
+  # drivers
+  ntfs3g # linux ntfs driver
+  # Office
+  onlyoffice-bin
+  #Browsers
+  brave
+  chromium
+  # GUI Software
+  github-desktop
+  discord
+  gimp
+  thunderbird
+  steam
+  protonup-qt
+  obsidian
+  notion-app-enhanced
+  asar # to make notion work again on linux https://github.com/notion-enhancer/notion-enhancer/issues/812
+];
+
+  
+  # --- Program Configurations ---
+
+  programs.fish.enable = true;
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # Steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+	
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "steam"
+    "steam-original"
+    "steam-run"
+  ];
+
+
+  # --- Services ---
+  
+  # List services that you want to enable:
+  
+  # Firmware Updates
+  services.fwupd.enable = true; # Use with fwupdmgr get-devices, refresh, get-updates, update
+
+  services.flatpak.enable = true;  
+    
+      
+        
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
